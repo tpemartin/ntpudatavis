eed6fd2cc6d5a31a5c8b34435b4c47bea9350a86
tpemartin <tpemartin@gmail.com>
2024-12-03 19:48:23 +0800

    update

--- R/rdata.R
+++ R/rdata.R
@@ -5,4 +5,20 @@
 #' @format A data frame with 22 rows and 5 columns.
 #' @name tw_shp_crop
 #'
+NULL
+#' 2020臺灣總統大選投票資料
+#'
+#' A detailed description of what the data frame contains and any other necessary details.
+#'
+#' @format A data frame with 66 rows and 18 columns.
+#' @name tw_shp_crop_election2020
+#'
+NULL
+#' 2024臺灣總統大選投票資料
+#'
+#' A detailed description of what the data frame contains and any other necessary details.
+#'
+#' @format A data frame with 66 rows and 18 columns.
+#' @name tw_shp_crop_election2024
+#'
 NULL  # End the documentation block with a NULL
