f92a9864beb4405b1eac9bd65788e87555eddabd
tpemartin <tpemartin@gmail.com>
2024-12-03 14:01:19 +0800

    增加台灣本島縣市simple feature

---
+++ R/rdata.R
@@ -0,0 +1,8 @@
+#' 臺灣本島cropped out simple feature
+#'
+#' A detailed description of what the data frame contains and any other necessary details.
+#'
+#' @format A data frame with 22 rows and 5 columns.
+#' @name tw_shp_crop
+#'
+NULL  # End the documentation block with a NULL
